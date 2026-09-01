f9d511cda075fa2ca6023ac2f91c81c43d018323
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-01 04:30:12 +0100

    Create Bash.zsh

---
+++ Bash.zsh
@@ -0,0 +1,8 @@
+# Build the minimal Docker image
+docker build -t aura-node:aot-scratch .
+
+# Check the image size (typically ~15MB - 30MB)
+docker images aura-node:aot-scratch
+
+# Run the containerized AOT binary
+docker run --rm aura-node:aot-scratch
