69e0d0cd8e30e62a9a04b94e3a109a309fddb320
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-04 21:52:01 +0100

    Update and rename Bash.zsh to Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -6,3 +6,8 @@ docker images aura-node:aot-scratch
 
 # Run the containerized AOT binary
 docker run --rm aura-node:aot-scratch
+git clone [https://github.com/auraecosystem/fsharp.git](https://github.com/auraecosystem/fsharp.git)
+cd fsharp
+dotnet restore
+dotnet build
+
